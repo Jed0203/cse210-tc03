31132cd16e90812974f0537ba3c833194e1a112a
calebbarzee <barzeec@gmail.com>
2021-10-01 18:17:40 -0600

    jed and elena file imports

--- Team Challenge/main_file.py
+++ Team Challenge/main_file.py
@@ -1,6 +1,10 @@
 import caleb_function
 import yang
+import jed
+import leon
 print("Welcome to our collaborative program.")
 
 caleb_function.print_function()
 yang.function()
+leon.print_function()
+jed.hello()
